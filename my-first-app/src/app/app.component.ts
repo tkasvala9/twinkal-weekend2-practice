a8976c34c6fcb3176db0590f1a6ac048ed7c62b3
tkasvala9 <twinkalkasvala@gmail.com>
2022-03-06 11:48:40 +0530

    create-table

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -10,15 +10,16 @@ export class AppComponent {
   title = 'my-first-app';
   
   constructor(public us:UserDataServiceService){}
-  sdate:any;
-  edate:any;
-  price:any;
-  calculate(sdate:any,edate:any){
-    this.sdate=new Date(sdate).getTime();
-    this.edate=new Date(edate).getTime();
-    let days=(this.edate-this.sdate)/(1000*3600*24);
-    this.price=2000;
-    console.log("total amount for trip of"+" "+days+" "+"days is"+" " +(this.price)*days);
+  numarray:any=[]
+  t:any;
+  i:any;
+  cratetable(value:any){
     
+    for(let i=1;i<=10;i++){
+      this.t=(value*i);
+      this.numarray[i]=value +" "+"*"+" "+i+" "+"="+" "+this.t;
+      console.log(this.numarray[i]);
+        }
+        
   }
 }
